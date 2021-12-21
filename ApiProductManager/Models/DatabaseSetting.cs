921f27d51266a69cd7496aea3e003ab2912f3128
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-12-21 01:41:29 -0500

    update files

--- ApiProductManager/Models/DatabaseSetting.cs
+++
@@ -1,8 +0,0 @@
-using pmApi.Models;
-
-public class DatabaseSetting
-{
-    public string ConnectionString { get; set; } = null!;
-    public string DatabaseName { get; set; } = null!;
-    public string ProductCollections { get; set; } = null!;
-}
