b4c7ed77381663e0f744a71a0c3b879bb4b79e53
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-12-21 01:17:26 -0500

    update files

---
+++ NetCoreAPIPostgreSQL/NetCoreAPIPostgreSQL.Data/Repositories/CarRepository.cs
@@ -0,0 +1,37 @@
+﻿using NetCoreAPIPostgreSQL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreAPIPostgreSQL.Data.Repositories
+{
+    public class CarRepository : ICarRepository
+    {
+        public Task<bool> DeleteCard(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Car>> GetAllCars()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Car> GetCarDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> InsertCard(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCard(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NetCoreAPIPostgreSQL/NetCoreAPIPostgreSQL.Model/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreAPIPostgreSQL.Model
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Doors { get; set; } 
+    }
+}
